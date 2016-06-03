c0d900a025800a4c9049827278a801c914eb1122
dramanat <dramanat459@gmail.com>
2016-06-03 08:53:20 -0500

    add blocks with args & lambdas

--- scopes/scopes.rb
+++ scopes/scopes.rb
@@ -7,3 +7,25 @@ def flower2(&blk)
   @text = 'flower'
   blk.call
 end
+
+def pass_var_1
+  yield "money"
+end
+
+def pass_var_2
+  yield "money" "moral economy"
+end
+
+def math_mtd(function: "sum")
+  math = case function
+         when "sum"
+           ->(a,b) { a+b }
+         when "subtraction"
+           ->(a,b) { a-b }
+         when "multiplication"
+           ->(a,b) { a*b }
+         else
+           "invalid"
+         end
+  math == "invalid" ? math : math.call(3,4)
+end
--- scopes/spec/scopes_spec.rb
+++ scopes/spec/scopes_spec.rb
@@ -12,3 +12,39 @@ describe "@text variable dragged to block scope" do
     end
   end
 end
+
+describe "pass arg to block" do
+  context "using do/end for block" do
+    it "should equal money makes the world go round" do
+      pass_var_1 do |m| "#{m} makes the world go round" end
+    end
+    it "should ignore 2nd arg and equal money makes the world go round" do
+      pass_var_2 do |m| "#{m} makes the world go round" end
+    end
+  end
+  context "using {} for block" do
+    it "should equal money makes the world go round" do
+      pass_var_1 {|m| "#{m} makes the world go round"}
+    end
+    it "should ignore 2nd arg and equal money makes the world go round" do
+      pass_var_2 {|m| "#{m} makes the world go round"}
+    end
+  end
+end
+
+describe "lambda usage" do
+  context "#math_mtd" do
+    it "should add and return 7" do
+      math_mtd.should == 7
+    end
+    it "should subtract and return -1" do
+      math_mtd(function: "subtraction").should == -1
+    end
+    it "should multiply and return 12" do
+      math_mtd(function: "multiplication").should == 12
+    end
+    it "should return invalid string" do
+      math_mtd(function: "division").should == 'invalid'
+    end
+  end
+end
